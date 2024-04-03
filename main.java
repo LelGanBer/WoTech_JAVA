b878779fdebd48e0a285bf6939f88baab8a76376
LelGanBer <165931718+LelGanBer@users.noreply.github.com>
2024-04-03 19:19:29 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,25 @@
+public class Main {
+  public static void main(String[] args) {    
+    
+
+    double temp = -10.23;
+
+    if (temp <= 5) {
+      System.out.println("Wear super warm");
+    }
+    else if (temp <= 15) {
+      System.out.println("Wear warm");
+    }
+    else if (temp <= 30) {
+      System.out.println("Wear normal");
+    }
+    else {
+      System.out.println("You need cooling");
+    }
+
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
